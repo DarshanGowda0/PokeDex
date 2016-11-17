83b184aec7834c23b8f81651d7fadd826d59ed51
darshan <darshan.gowda008@gmail.com>
2016-11-17 13:10:10 +0530

    implemented the view controller for collection view

--- PokeDex/ViewController.swift
+++ PokeDex/ViewController.swift
@@ -8,12 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
     }
-
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell{
+            
+            
+            let pokemon = Pokemon(name: "Pokemon \(indexPath.row+1)", pokedexID: indexPath.row+1)
+            cell.configureCell(pokemon: pokemon)
+            return cell
+        }else{
+            return UICollectionViewCell()
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    
+    
 }
 
